e1533a1cd15e5508118179418ad8c43dc2616034
Slime <4sliman4@gmail.com>
2025-04-19 05:17:15 +0300

    Make it a PWA

---
+++ public/service_worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'intear-wallet-{{{VERSION}}}';
+const DOMAIN = self.location.hostname;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.add('/');
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  // Don't cache API requests, wallet.intear.tech only hosts static files
+  const shouldCache = url.hostname === DOMAIN;
+
+  if (shouldCache) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+
+          const fetchRequest = event.request.clone();
+
+          return fetch(fetchRequest)
+            .then((response) => {
+              if (!response || response.status !== 200) {
+                return response;
+              }
+
+              const responseToCache = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseToCache);
+                });
+
+              return response;
+            });
+        })
+    );
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
