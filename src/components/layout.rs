f3c48909db0fc0d8a896c7ef26985fae83f81297
Slime <4sliman4@gmail.com>
2025-05-28 23:15:27 +0300

    Convert backgrounds to webp to compress them

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -32,7 +32,7 @@ fn left_edge_threshold() -> f64 {
 fn get_random_background() -> String {
     let mut rng = OsRng;
     let random_num = rng.gen_range(1..=3);
-    format!("/bg{}.png", random_num)
+    format!("/bg{}.webp", random_num)
 }
 
 #[derive(Clone, Copy, PartialEq, Debug)]
